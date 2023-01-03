17023bf060b5106ab3f4c7e69df5d2cb10cb37ba
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-01-03 10:38:43 -0800

    update js

---
+++ script.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+const section = document.createElement('section');
+body.appendChild(section);
+section.innerHTML = `
+<h3>BMW</h3>
+<img class="card-image" src="car.jpg" alt="" srcset="">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti distinctio, dolore in alias doloremque
+   quo vero saepe provident molestiae hic iste, eligendi ullam ad aperiam beatae necessitatibus velit nisi. Alias?</p>
+  
+   <button type="button">See more</button>
+  `;
+
