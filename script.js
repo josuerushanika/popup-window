16ffd24ce56f6f9f74a0910dbd9c0b4f87c90e7a
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-01-03 12:36:23 -0800

    Add js

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ section.innerHTML = `
    <button type="button">See more</button>
   `;
 
+  const open = document.querySelector('.see-more');
+  open.addEventListener('click',() => {
+    const main = document.createElement('div');
+    const popup = document.createElement('div');
+    popup.innerHTML =`<h3>BMW</h3>
+    <img class="card-image" src="car.jpg" alt="" srcset="">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti distinctio, dolore in alias doloremque
+       quo vero saepe provident molestiae hic iste, eligendi ullam ad aperiam beatae necessitatibus velit nisi. Alias?</p>
+      
+       <button class = "see-more" type="button">close</button>
+    `;
+  })
+
+
